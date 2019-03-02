8001414df2cb72bf094d1e6cff5fb8bd2cc011bb
Marcin <marcingladkowski@gmail.com>
2019-03-02 14:22:21 +0100

    create game model

---
+++ src/scripts/model/game.js
@@ -0,0 +1,11 @@
+export default class Game {
+
+    /*
+    * @param teams = []
+    * @param questions QuestionStore
+    */
+    constructor(teams, questionsStore) {
+        this.teams = teams;
+        this.questionsStore = questionsStore;
+    }
+}
