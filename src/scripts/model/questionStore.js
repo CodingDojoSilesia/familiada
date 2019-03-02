c88a89dea8320238ab306e94e3d3419c928d4926
Marcin <marcingladkowski@gmail.com>
2019-03-02 14:22:53 +0100

    return parse random question

--- src/scripts/model/questionStore.js
+++ src/scripts/model/questionStore.js
@@ -1,35 +1,29 @@
-class QuestionStore {
+import Answer from '../model/answer';
+import Question from '../model/question';
+
+export default class QuestionStore {
 
     constructor(questions) {
-        this.questions = questions;
+        this.questions = this.parseQuestions(questions);
     }
 
-    parseQuestions() {
-        // return Object.values(this.questions);
+    parseQuestions(questions) {
+        const titles = Object.keys(questions);
+        const answers = Object.values(questions);
+        const parsedQuestions = [];
 
-        for (let i = 0; i < Object.keys(questions).length; i++) {
-
+        for (let i = 0; i < answers.length; i++) {
+            const parsedAnswers = answers[i].filter(answer => answer).map(answer => new Answer(answer));
+            parsedQuestions.push(new Question(titles[i], parsedAnswers));
         }
+        return parsedQuestions;
     }
 
     getRandomQuestion() {
-
-        const title = this.getRandomQuestionTitle();
-        return true;
-    }
-
-    getRandomQuestionTitle() {
-        const questionsTitles = Object.keys(this.questions);
-        const randomNumber = Math.floor(Math.random() * (Object.keys(this.questions).length - 1 + 1) + 1);
-
-        return questionsTitles[randomNumber];
+        return this.questions[this.getRandomQuestionId()];
     }
     
-    getQuestions() {
-        return this.questions;
+    getRandomQuestionId() {
+        return Math.floor(Math.random() * (this.questions.length - 1 + 1) + 1);
     }
 }
-
-module.exports = {
-    QuestionStore,
-}    
