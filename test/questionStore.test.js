aefcc6921f6ea120da1f036f3633e80b56591559
Marcin <marcingladkowski@gmail.com>
2019-03-02 14:21:23 +0100

    reactor questionStore tests

--- test/questionStore.test.js
+++ test/questionStore.test.js
@@ -1,29 +1,13 @@
-import { QuestionStore } from '../src/scripts/model/questionStore';
+import QuestionStore from '../src/scripts/model/questionStore';
 import { questions } from '../src/data.json';
-import { Question } from '../src/scripts/model/question';
-import { Answer } from '../src/scripts/model/answer';
-import { exists } from 'fs';
+import Question from '../src/scripts/model/question';
+import Answer from '../src/scripts/model/answer';
 
-describe('returnRandomQuestionFromDataJson', () => {
-    test('should return random question', () => {
-
-        const questionsStoreTest = new QuestionStore(questions);
-        const questionRandomTitle = questionsStoreTest.getRandomQuestionTitle();
-
-        expect(typeof questionRandomTitle).toBe('string');
-    });
-
-    test('testReturnQuestionAsModels', () => {
-
+describe('testQuestionStore', () => {
+    test('testReturnRandomQuestion', () => {
         const questionsStore = new QuestionStore(questions);
-        const questionsInGame = questionsStore.getQuestions();
-    });
+        const randomQuestion = questionsStore.getRandomQuestion();
 
-    test('testParseQuestions', () => {
-        for (let i = 0; i < Object.keys(questions).length; i++) {
-            const answers = Object.values(questions)[i];
-            const parsedAnswers = answers.filter(answer => answer).map(answer => new Answer(answer));
-            const question = new Question(Object.keys(questions)[i], parsedAnswers)
-        }
+        expect(typeof randomQuestion).toBe('object');
     });
 });
