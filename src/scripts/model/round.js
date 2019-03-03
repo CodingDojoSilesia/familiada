73797fef4e9bac459c6ae150847a2a304cadb2d6
Marcin <marcingladkowski@gmail.com>
2019-03-03 12:26:13 +0100

    method to disable unused board rows on round

--- src/scripts/model/round.js
+++ src/scripts/model/round.js
@@ -1,7 +1,22 @@
+import board from '../board';
+
 export default class Round {
 
     constructor(question) {
         this.question = question;
+        this.disableUnusedBoardRows();
     }
 
+    getQuestion() {
+        return this.question;
+    }
+
+    /**
+     * Remove unused board answers fields for question
+     */
+    disableUnusedBoardRows() {
+        for (let i = 6; i > this.getQuestion().getAnswers().length; i--) { 
+            board.hideUnusedAnswerField(i);
+        }
+    }
 }
