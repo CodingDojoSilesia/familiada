4072061169b354eeabd46e66617b7a7d20a82667
Marcin <marcingladkowski@gmail.com>
2019-03-03 12:24:11 +0100

    refactor test questionStore

--- test/questionStore.test.js
+++ test/questionStore.test.js
@@ -4,10 +4,25 @@ import Question from '../src/scripts/model/question';
 import Answer from '../src/scripts/model/answer';
 
 describe('testQuestionStore', () => {
+
+    let questionsStore = null;
+
+    beforeAll(() => {
+        questionsStore = new QuestionStore(questions);
+    });
+
     test('testReturnRandomQuestion', () => {
-        const questionsStore = new QuestionStore(questions);
         const randomQuestion = questionsStore.getRandomQuestion();
+        const firstAnswersKeysFromRandomQuestion = Object.keys(randomQuestion.getAnswers()[0]);
 
         expect(typeof randomQuestion).toBe('object');
+        expect(Array.isArray(randomQuestion.getAnswers())).toBe(true);
+        expect(firstAnswersKeysFromRandomQuestion.length).toBe(3);
+        expect(firstAnswersKeysFromRandomQuestion[0]).toBe('ans');
+        expect(firstAnswersKeysFromRandomQuestion[1]).toBe('lp');
+        expect(firstAnswersKeysFromRandomQuestion[2]).toBe('points');
+    });
+    test('testReturnParsedArrayOfQuestions', () => {
+
     });
 });
