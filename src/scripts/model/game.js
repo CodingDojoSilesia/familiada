d03864d024c8d259185a7e135a2b7cdf27d2fffc
Marcin <marcingladkowski@gmail.com>
2019-03-03 12:25:00 +0100

    add getters to game model

--- src/scripts/model/game.js
+++ src/scripts/model/game.js
@@ -1,3 +1,6 @@
+import Round from "./round";
+import TEAMS from '../teams';
+
 export default class Game {
 
     /*
@@ -7,5 +10,27 @@ export default class Game {
     constructor(teams, questionsStore) {
         this.teams = teams;
         this.questionsStore = questionsStore;
+        this.round = new Round(this.questionsStore.getRandomQuestion());
+        this.setRandomTeam();
+    }
+
+    setRandomTeam() {
+        this.setCurrentTeam(this.teams[Math.round(Math.random())].name);
+    }
+
+    setCurrentTeam(teamName) {
+        this.currentTeam = teamName;
+    }
+
+    getCurrentTeam() {
+        return this.currentTeam;
+    }
+
+    getRound() {
+        return this.round;
+    }
+
+    getTeams() {
+        return this.teams;
     }
 }
