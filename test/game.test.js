70af8e662e4b81a6edc3f84b940dc126c437d66f
Marcin <marcingladkowski@gmail.com>
2019-03-03 12:26:56 +0100

    test how choose good player answer answer

--- test/game.test.js
+++ test/game.test.js
@@ -4,13 +4,37 @@ import Question from '../src/scripts/model/question';
 import Answer from '../src/scripts/model/answer';
 import Game from '../src/scripts/model/game';
 import Team from '../src/scripts/model/team';
-import teams from '../src/scripts/teams';
+import TEAMS from '../src/scripts/teams';
 
-describe('testQuestionStore', () => {
-    test('testReturnRandomQuestion', () => {
-  
-        const game = new Game([new Team(teams.BLUE), new Team(teams.RED)], new QuestionStore(questions));
+describe('testOnNewGame', () => {
 
+    let game = null;
+
+    beforeAll(() => {
+        game = new Game([new Team(TEAMS.BLUE), new Team(TEAMS.RED)], new QuestionStore(questions));
+    });
+
+    test('testCreatedNewGame', () => {
         expect(typeof game).toBe('object');
+    });
+
+    test('testFindGoodAnswerFromQuestionAswers', () => {
+
+        const questionAnswers = ['Odpowiedz', 'Odpowiedz2', 'Odpowiedz3'];
+
+        const playerSpeechAnswer = [
+            {confidence: 0.9631079435348511, transcript: "odpowiedz"},
+            {confidence: 0.9631079435348511, transcript: "cos innego"}
+        ];
+
+        const parsedQuestionAnswers = questionAnswers.map(answer => answer.toLowerCase());
+
+        const result = playerSpeechAnswer.find(answer => { return parsedQuestionAnswers.includes(answer.transcript)});
+
+        expect(result.transcript).toBe('odpowiedz');
+    });
+
+    test('testSetRandomTeamAsCurrentWhenStartGame', () => {
+        expect(Object.values(TEAMS).includes(game.getCurrentTeam())).toBe(true);
     });
 });
