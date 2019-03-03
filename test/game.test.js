e3958840b9a3d641575a923127cfb3ad7aa78152
Martin89PL <marcingladkowski@gmail.com>
2019-03-04 00:11:05 +0100

    reactor game tests

--- test/game.test.js
+++ test/game.test.js
@@ -1,5 +1,5 @@
 import QuestionStore from '../src/scripts/model/questionStore';
-import { questions } from '../src/data.json';
+// import { questions } from '../src/data.json';
 import Question from '../src/scripts/model/question';
 import Answer from '../src/scripts/model/answer';
 import Game from '../src/scripts/model/game';
@@ -7,6 +7,22 @@ import Team from '../src/scripts/model/team';
 import TEAMS from '../src/scripts/teams';
 
 describe('testOnNewGame', () => {
+
+    const questions = { 'Atrybut świętego Mikołaja?': 
+        [ 
+            null,
+            { ans: 'Worek', lp: 1, points: 35 },
+            { ans: 'Czapka', lp: 2, points: 23 },
+            { ans: 'Broda', lp: 3, points: 20 },
+            { ans: 'Dzwonek', lp: 4, points: 8 },
+            { ans: 'Renifery', lp: 5, points: 7 } 
+        ]
+    };
+
+    const playerSpeechAnswer = [
+        {confidence: 0.9631079435348511, transcript: "worek"},
+        {confidence: 0.9631079435348511, transcript: "cos innego"}
+    ];
 
     let game = null;
 
@@ -19,22 +35,28 @@ describe('testOnNewGame', () => {
     });
 
     test('testFindGoodAnswerFromQuestionAswers', () => {
+        const result = game.resolvePlayerAnswer(playerSpeechAnswer[0].transcript);
+        expect(typeof result).toBe('object');
+    });
 
-        const questionAnswers = ['Odpowiedz', 'Odpowiedz2', 'Odpowiedz3'];
-
-        const playerSpeechAnswer = [
-            {confidence: 0.9631079435348511, transcript: "odpowiedz"},
-            {confidence: 0.9631079435348511, transcript: "cos innego"}
-        ];
-
-        const parsedQuestionAnswers = questionAnswers.map(answer => answer.toLowerCase());
-
-        const result = playerSpeechAnswer.find(answer => { return parsedQuestionAnswers.includes(answer.transcript)});
-
-        expect(result.transcript).toBe('odpowiedz');
+    test('testReturnFalseWhenAnswerIsBad', () => {
+        const result = game.resolvePlayerAnswer('bledna odpowiedz');
+        expect(result).toBe(false);
     });
 
     test('testSetRandomTeamAsCurrentWhenStartGame', () => {
-        expect(Object.values(TEAMS).includes(game.getCurrentTeam())).toBe(true);
+        expect(Object.values(TEAMS).includes(game.getCurrentTeam().getName())).toBe(true);
+    });
+
+    test('testSwitchCurrentTeamOnRound', () => {
+        
+        const initialTeamName = game.getCurrentTeam().getName();
+
+        game.switchCurrentTeam();
+        
+        const changedTeamName = game.getCurrentTeam().getName();
+
+        expect(initialTeamName === changedTeamName).toBe(false);
+
     });
 });
