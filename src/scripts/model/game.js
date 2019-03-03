ddd5ec35d95452a02a962ff6d65fb1ac49f2bbe4
Martin89PL <marcingladkowski@gmail.com>
2019-03-04 00:10:45 +0100

    simple parsed user answers

--- src/scripts/model/game.js
+++ src/scripts/model/game.js
@@ -1,5 +1,6 @@
 import Round from "./round";
 import TEAMS from '../teams';
+import board from '../board';
 
 export default class Game {
 
@@ -14,6 +15,47 @@ export default class Game {
         this.setRandomTeam();
     }
 
+    handlePlayerAnswer(playerAnswer) {
+        const result = this.resolvePlayerAnswer(playerAnswer);
+        
+        const currentTeam = this.getCurrentTeam();
+        
+        if (result &&
+            currentTeam.getErrors() < 3 &&
+            this.round.getStatus() === 'default'
+        ) {
+            this.round.setBoardAnswer(result);
+        } else {
+            currentTeam.addError();
+            this.round.setError(currentTeam);
+        }
+
+        if (this.round.checkFinish()) {
+            // @TODO clear board, add points to team
+            this.round = new Round(this.questionsStore.getRandomQuestion());
+            return;
+        }
+        
+        if (currentTeam.getErrors() === 3) {
+            this.switchCurrentTeam();
+        }
+
+        return;
+    }
+
+    resolvePlayerAnswer(playerAnswer) {
+
+        const result =  this.getRound().getQuestion().getAnswers().find(answer => {
+            return answer.ans.toLowerCase() === board.removeDiacritics(playerAnswer);
+        });
+
+        return (result !== undefined) ? result : false;
+    }
+
+    switchCurrentTeam() {
+        this.setCurrentTeam(this.teams.find(team => team.getName() !== this.currentTeam).name)
+    }
+
     setRandomTeam() {
         this.setCurrentTeam(this.teams[Math.round(Math.random())].name);
     }
@@ -23,7 +65,7 @@ export default class Game {
     }
 
     getCurrentTeam() {
-        return this.currentTeam;
+        return this.teams.find(team => team.getName() === this.currentTeam);
     }
 
     getRound() {
