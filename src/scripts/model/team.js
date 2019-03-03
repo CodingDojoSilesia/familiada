bbeb6dd3ae55e0688357f527ee8ce3a862ecd5e5
Marcin <marcingladkowski@gmail.com>
2019-03-03 12:26:32 +0100

    method to add points and errors

--- src/scripts/model/team.js
+++ src/scripts/model/team.js
@@ -2,5 +2,14 @@ export default class Team {
     constructor(name) {
         this.name = name;
         this.points = 0;
+        this.errors = 0;
+    }
+
+    addPoints(points) {
+        this.points += points;
+    }
+
+    addError() {
+        this.errors += 1;
     }
 }
