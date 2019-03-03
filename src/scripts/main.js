089c78fdf357febda890aef436650e5287d7f016
Marcin <marcingladkowski@gmail.com>
2019-03-03 12:24:44 +0100

    simple recognize user answer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,10 @@ import TEAMS from './teams'
 import board from './board';
 import speech from './speech';
 import { questions } from './../data.json';
+import Game from './model/game';
+import Team from './model/team';
+import QuestionStore from './model/questionStore';
+
 
 board.setAnswer(1, '', 0);
 board.setAnswer(2, '', 0);
@@ -16,11 +20,26 @@ board.setPoints(TEAMS.RED, 0);
 board.setErrors(TEAMS.RED, 0);
 board.setErrors(TEAMS.BLUE, 0);
 
-board.setQuestion('Coding Dojo Silesia');
+const game = new Game([new Team(TEAMS.BLUE), new Team(TEAMS.RED)], new QuestionStore(questions));
+board.setQuestion(game.getRound().getQuestion().getName());
 
-speech.loadGrammar(['żółty', 'zielony', 'fioletowy', 'niebieski']);
+speech.loadGrammar(game.getRound().getQuestion().getAnswersWords());
 document.querySelector('.record').onclick = function() {
     speech.start().then((result) => {
-        console.log(result);
+
+        const playerSpeechAnswer = result[0][0].transcript;
+
+        const answerForQuestion = game.getRound().getQuestion().getAnswers();
+
+        const playerAnswerResult = answerForQuestion.find(answer => {
+            return answer.ans.toLowerCase() === playerSpeechAnswer;
+        });
+
+        if (playerAnswerResult != undefined) {
+            board.setAnswer(playerAnswerResult.lp, playerAnswerResult.ans, playerAnswerResult.points);
+            return true;
+        }
+
+        board.setErrors(game.getTeam(), 1);
     });
 };
