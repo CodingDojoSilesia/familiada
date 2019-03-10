d471007c9c91d7fa2159081c7bedf02b6ce92e0f
Martin89PL <marcingladkowski@gmail.com>
2019-03-10 11:17:35 +0100

    add testing document

---
+++ test/documentForTests.js
@@ -0,0 +1,61 @@
+export const exampleDomDocument = `
+<body class="familiada first-part">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-2 text-center blue-team team">
+            <h4 class="name">NIEBIESCY</h4>
+            <div class="fail">X</div>
+            <div class="fail">X</div>
+            <div class="fail">X</div>
+            <h3>SUMA: <span class="team-points">0</span></h3>
+        </div>
+        <div class="col-lg-8 text-center">
+            <h1>FAMILIADA</h1>
+            <h3 class="question">Wyraz ktory nie ma liczby pojedynczej?</h3>
+            <ul class="answers">
+                <li data-answer-num="1">
+                    <span class="number">1.</span>
+                    <span class="text">      Drzwi.. ... ... ... ... ... ... ... ... ... ... ... </span>
+                    <span class="points">22</span>
+                </li>
+                <li data-answer-num="2">
+                    <span class="number">2.</span>
+                    <span class="text">      Spodnie ... ... ... ... ... ... ... ... ... ... ... </span>
+                    <span class="points">20</span>
+                </li>
+                <li data-answer-num="3">
+                    <span class="number">3.</span>
+                    <span class="text">      Nozyce. ... ... ... ... ... ... ... ... ... ... ... </span>
+                    <span class="points">16</span>
+                </li>
+                <li data-answer-num="4">
+                    <span class="number">4.</span>
+                    <span class="text">      Skrzypce... ... ... ... ... ... ... ... ... ... ... </span>
+                    <span class="points">14</span>
+                </li>
+                <li data-answer-num="5">
+                    <span class="number">5.</span>
+                    <span class="text">      Sanki.. ... ... ... ... ... ... ... ... ... ... ... </span>
+                    <span class="points">10</span>
+                </li>
+                <li data-answer-num="6">
+                    <span class="number">6.</span>
+                    <span class="text">      Urodziny... ... ... ... ... ... ... ... ... ... ... </span>
+                    <span class="points">8</span>
+                </li>
+            </ul>
+            <h4 class="record">Nagraj!</h4>
+            <h3 class="winner"></h3>
+        </div>
+        <div class="col-lg-2 text-center red-team team">
+            <h4 class="name">CZERWONI</h4>
+            <div class="fail">X</div>
+            <div class="fail">X</div>
+            <div class="fail">X</div>
+            <h3>SUMA: <span class="team-points">0</span></h3>
+        </div>
+    </div>
+</div>
+
+<script src="./assets/scripts/bundle.js"></script>
+</body>`
